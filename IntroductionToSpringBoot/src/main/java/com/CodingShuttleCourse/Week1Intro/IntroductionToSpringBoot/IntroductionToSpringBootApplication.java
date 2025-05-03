0316ba44766f457edb9bf4b379d32a7eaf77fb99
cswizard <rbcs74218@gmail.com>
2025-05-03 21:13:28 +0530

    init commit

---
+++ IntroductionToSpringBoot/src/main/java/com/CodingShuttleCourse/Week1Intro/IntroductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.CodingShuttleCourse.Week1Intro.IntroductionToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+	}
+
+}
